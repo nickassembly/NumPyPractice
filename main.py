f3b3f7de8562e834175d72d1aad4327db001d878
nickguerra <nickg@maliciousgames.com>
2023-02-25 06:57:53 -0600

    multi dimensional arrays

--- main.py
+++ main.py
@@ -1,13 +1,66 @@
 import numpy as np
 
-a = np.array([1,2,3])
-print(a)
+# l1 = [1,2,3]
+# l2 = [4,5,6]
+# a1 = np.array(l1)
+# a2 = np.array(l2)
 
-print(a[0])
-a[0] = 10
-print(a)
+# # Dot Products
 
-b = a * np.array([2,0,2])
-print(b)
+# # traditional way
+# dp = 0
+# for i in range(len(l1)):
+#     dp += l1[i] * l2[i]
+# print(dp)
 
-# python lists v arrays
+# # numpy way
+# dp = np.dot(a1,a2)
+# print(dp)
+
+# dp = a1 @ a2
+# print(dp)
+
+# # multidimensional arrays
+# a = np.array([[1,2], [3,4]])
+# print(a)
+# print(a.shape)
+
+# print(a[0,0])
+# print(a[:,0])
+# print(a[0,:])
+
+# print(np.linalg.inv(a)) #calc inverse
+# print(np.linalg.det(a)) #calc determinate
+
+# c = np.diag(a)
+# print(np.diag(c)) # calc diagonal
+
+# # slicing
+# print(a)
+
+# b = a[0,1:3] 
+# #b1 = a[a:,1] # only column 1
+# print(b)
+
+# a2 = np.array([[1,2], [3,4], [5,6]])
+# print(a2)
+
+# bool_idx = a2 > 2
+# print(bool_idx)
+# print(a2[bool_idx])
+
+# bb = np.where(a>2, a, -1)
+# print(bb)
+
+# fancy indexing
+# a = np.array([10,19,30,41,50,61])
+# print(a)
+# b = [1,3,5]
+# print(a[b])
+
+# print(a)
+# even = np.argwhere(a%2==0).flatten()
+# print(a[even])
+
+# reshaping arrays
+
