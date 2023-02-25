c0d3ab35a49daa8f6e8415a5bac60f9e037198a6
nickguerra <nickg@maliciousgames.com>
2023-02-25 13:05:46 -0600

    added numpy library functions around arrays

--- main.py
+++ main.py
@@ -64,3 +64,121 @@ import numpy as np
 
 # reshaping arrays
 
+# a = np.arange(1,7)
+# print(a)
+# print(a.shape)
+# b = a[np.newaxis, :]
+# c = a[:, np.newaxis]
+# print(c)
+# print(b)
+# print(b.shape)
+
+#concatenation
+
+# a = np.array([[1,2], [3,4]])
+# print(a)
+# b = np.array([[5,6]])
+# c = np.concatenate((a,b.T), axis=1) #need to transpose when using 1, can also do axis=None
+# print(c)
+
+#stacking arrays
+# a = np.array([1,2,3,4])
+# b = np.array([5,6,7,8])
+# #hstack
+# c = np.vstack((a,b))
+# print(c)
+
+# broadcasting
+# x = np.array([[1,2,3], [4,5,6], [1,2,3], [4,5,6]])
+# a = np.array([1,0,1],[1,0,1], [1,0,1], [1,0,1])
+# y = x + a
+# print(y)
+
+# a = np.array([[7,8,9,10,11,12,13], [17,18,19,20,21,22,23]])
+# print(a)
+# print(a.sum(axis=1)) # 1 sum entry for each row, 0 axis would be one sum over each column, can also use None
+# print(a.var(axis=None)) #variance
+# print(a.std(axis=None)) #standard deviation
+
+# datatypes
+# x = np.array([1.0,2.0], dtype=np.int64) #can pass in dtype to specify data type
+# print(x)
+# print(x.dtype)
+
+# # copying arrays
+# a = np.array([1,2,3])
+# b = a
+# b[0] = 42
+# print(b)
+# print(a) # only copied reference, and both objects point to same memory location, so modifies both arrays. To copy use a.copy
+
+# a = np.zeros((2,3))
+# print(a)
+
+# a = np.ones((2,3))
+# print(a)
+
+# a = np.full((2,3), 5.0)
+# print(a)
+
+# a = np.eye(3)
+# print(a)
+
+# a = np.arange(20)
+# print(a)
+
+# a = np.linspace(0,10,5)
+# print(a)
+
+#random numbers
+# a = np.random.random((3,2)) # generate 3 x 2 array with values between 0 and 1
+# print(a)
+
+# a = np.random.randn(1000) # normal / Gaussian
+# print(a.mean(), a.var())
+
+# a = np.random.randint(3,10, size=(3,3))
+# print(a)
+
+# linear algebra
+# eigenvalues PCA (principle component analysis)
+
+# a = np.array([[1,2], [3,4]])
+# eigenvalues, eigenvectors = np.linalg.eig(a)
+
+# # print(eigenvalues)
+# # print(eigenvectors) # column vector - col 0 corresponds to eigenvalue 0
+
+# # e_vec * e_val = A * e_vec (formula to verify)
+# b = eigenvectors[:,0] * eigenvalues[0]
+# print(b)
+# c = a @ eigenvectors[:,0]
+# print(b)
+
+# # compare values -- compare if 2 arrays are equal use np all close function
+# print(np.allclose(b,c))
+
+# solving linear systems 
+
+# A = np.array([[1,1], [1.5, 4.0]])
+# b = np.array([2200, 5050])
+
+# # not a great way, slow and has numerical issues
+# # x = np.linalg.inv(A).dot(b) # inverse of A times b, dot product of A and b
+# # print(x)
+
+# # better way same result but faster
+# x = np.linalg.inv(A).dot(b)
+# print(x)
+
+# x = np.linalg.solve(A, b)
+# print(x)
+
+# load CSV files
+# np.loadtxt, np.genfromtxt (either one works)
+data = np.loadtxt('spambase.csv', delimiter=",", dtype=np.float32)
+print(data.shape)
+
+
+
+
